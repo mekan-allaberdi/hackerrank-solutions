47ccbddbae82239d7ce6a757d00f2fe8c3deca54
Mekan Allaberdiyev <mekan.allaberdi@gmail.com>
2018-03-05 11:58:16 +0500

    Update hash_table.py

--- Data structures/hash_table.py
+++ Data structures/hash_table.py
@@ -1,5 +1,34 @@
 # python3
+'''
+  Problem : 
+  Given the words in the magazine and the words in the ransom note, 
+  print Yes if he can replicate his ransom note exactly using whole words from the magazine; 
+  otherwise, print No.
 
+  input :
+    <n>: number of words in the magazine, <m>: number of words in the ransom note
+    <magazine_words>
+    <ransome note words>
+    
+  output:
+    Print Yes if he can use the magazine to create an untraceable replica of his ransom note; otherwise, print No.
+  
+  Sample Input 0
+  6 4
+  give me one grand today night
+  give one grand today 
+  
+  Sample Output 0
+  Yes
+
+  Sample Input 1
+  6 5
+  two times three is not four
+  two times two is four
+  
+  Sample Output 1
+  No
+'''
 def ransom_note(magazine, ransom):
     d = dict()
     for w in magazine:
